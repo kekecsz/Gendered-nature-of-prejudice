8125abec4d9961be4298f6b2bd70d65ea298852b
kekecsz <kekecs.zoltan@gmail.com>
2018-02-16 12:29:23 +0100

    Analysis code for the pilot dataset

---
+++ PSA - GNP prior study analysis script.R
@@ -0,0 +1,50 @@
+
+####################################################
+#              Load packages                       #
+####################################################
+
+library(lme4) # approach 1, for mixed effects anova
+library(lmerTest) # approach 1, for mixed effects anova (to do significance test)
+library(cocor) # approach 2, compare two correlations to each other 
+
+####################################################
+#              Load data file                      #
+####################################################
+
+# load pilot data
+dataset = read.csv("https://raw.githubusercontent.com/kekecsz/Gendered-nature-of-prejudice/master/dataset_pilot.csv")
+
+###########################################################
+#              Statistical analysis                       #
+###########################################################
+
+### Approach 1: ANOVA looking for race * sex interaction
+
+## data management
+
+# creating a long format from a wide format
+warmth = c(dataset$Therm_8, dataset$Therm_9, dataset$Therm_14, dataset$Therm_15)
+race = rep(c("black", "white"), each = 2*length(dataset$Therm_8))
+sex = c(rep(c("men", "women"), each = length(dataset$Therm_8)), rep(c("men", "women"), each = length(dataset$Therm_8)))
+ID = rep(paste("ID_", 1:length(dataset$Therm_8), sep = ""), 4)
+data = as.data.frame(warmth)
+data = as.data.frame(cbind(data, ID, race, sex))
+
+# specifying factors
+data$ID = factor(data$ID)
+data$race = factor(data$race)
+data$sex = factor(data$sex)
+
+# statistical test
+mod = lmer(warmth ~ race * sex + (1|ID), data = data)
+# results
+summary(mod)
+
+
+
+### Approach 2: comparing the correlations of AW_B and AWM_BM vs. the correlation of AW_B and AWF_BF. We assume that the corelation of AW_B and AWM_BM would be higher than the correlation of AW_B and AWF_BF.
+
+# statistical test
+cor_results = cocor(~AW_B + AWM_BM | AW_B + AWF_BF, alternative = "greater", data = dataset)
+# results
+cor_results
